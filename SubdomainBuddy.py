3c6c412958e944a2f2953e2e452eec088df16a05
tavgar <104298517+tavgar@users.noreply.github.com>
2023-02-25 20:10:59 +0100

    Update SubdomainBuddy.py

--- SubdomainBuddy.py
+++ SubdomainBuddy.py
@@ -3,7 +3,7 @@ import argparse
 import concurrent.futures
 import dns.resolver
 
-def check_subdomain(subdomain):
+def check_subdomain(subdomain, Thread=True):
     subdomain = subdomain.strip()  # Remove any trailing whitespace
 
     # Determine whether the URL should be http or https
@@ -18,7 +18,11 @@ def check_subdomain(subdomain):
     # Perform a DNS lookup for the subdomain
     try:
         answers = dns.resolver.resolve(subdomain)
-        print(f"DNS resolution for {subdomain}: {[str(rdata) for rdata in answers]}")
+        for rdata in answers:
+            if isinstance(rdata, dns.rdtypes.ANY.CNAME):
+                print(f"CNAME record found for {subdomain}: {rdata.target}")
+            else:
+                print(f"DNS resolution for {subdomain}: {rdata}")
     except:
         print(f"Unable to perform DNS resolution for {subdomain}")
 
@@ -31,9 +35,8 @@ def check_subdomain(subdomain):
             print(f"Response code for {subdomain}: {response.status_code}")
     except:
         print(f"Unable to connect to {url}")
-
-    print("-" * 50)  # Print a line between each subdomain
-
+    if(Thread == False):
+       print("_"*50)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Check for subdomain takeover')
     parser.add_argument('--file', dest='file', help='The file containing a list of subdomains', required=False)
@@ -44,11 +47,17 @@ if __name__ == '__main__':
     subdomains_file = args.file
     with open(subdomains_file, "r") as file:
         subdomains = file.readlines()
-
+        print("_"*50)
     # Create a concurrent executor to execute the subdomain checks in parallel
     if args.thready:
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            executor.map(check_subdomain, subdomains)
+    threads = []
+    for subdomain in subdomains:
+        t = threading.Thread(target=check_subdomain, args=(subdomain,))
+        t.start()
+        threads.append(t)
+
+    for t in threads:
+        t.join()
     else:
-        with concurrent.futures.ProcessPoolExecutor() as executor:
-            executor.map(check_subdomain, subdomains)
+        for subdomain in subdomains:
+            check_subdomain(subdomains, Thread=False)
